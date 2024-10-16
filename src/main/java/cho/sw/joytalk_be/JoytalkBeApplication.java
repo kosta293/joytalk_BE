4c2392b3eadbecc1e578da83becb0432a7518832
bbman9900 <tmddn1679@naver.com>
2024-10-16 17:45:28 +0900

    first commit

---
+++ src/main/java/cho/sw/joytalk_be/JoytalkBeApplication.java
@@ -0,0 +1,13 @@
+package cho.sw.joytalk_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoytalkBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoytalkBeApplication.class, args);
+    }
+
+}
